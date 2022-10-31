705390b4eab7515ab09a0a4a2fd7d7f93d8c064d
unknown <22220252@KTNG.AD>
2022-10-31 17:57:20 +0900

    점수계산

--- socket.js
+++ socket.js
@@ -2,7 +2,8 @@ const SocketIO = require("socket.io");
 module.exports = (server) => {
   //const io = SocketIO(server);
   const io = SocketIO(server, { path: "/socket.io/tit-for-tat/" });
-  //let playerNoIdx = 0;
+  const playResultInit = { isP1End: false, p1NickName: "", p1Choice: "", p1Score: 0, isP2End: false, p2NickName: "", p2Choice: "", p2Score: 0 }; // 결과 초기화용
+  let playResultInfo = {}; // 이번라운드결과
 
   io.on("connection", (socket) => {
     console.log("connection", socket.data);
@@ -25,6 +26,52 @@ module.exports = (server) => {
     socket.on("enter game", (nickName) => {
       console.log("enter game", nickName);
       enterGameProcess(nickName);
+    });
+
+    /**
+     * on play game
+     * @param  playInfo       { round: 1, nickName: "", playerNo: 1, choice: "" }; // 개별선택정보
+     * @return playResultInfo { round: 1, nextRound:1, isP1End:false, p1NickName:"", p1Choice: "", p1Score: 0, isP2End: false, p2NickName:"", p2Choice: "", p2Score: 0 }; // 개별선택취합결과
+     */
+    socket.on("play game", (playInfo) => {
+      console.log("play game", playInfo);
+      if (playInfo.playerNo === 1) {
+        playResultInfo.round = playInfo.round;
+        playResultInfo.nextRound = playInfo.round;
+        playResultInfo.isP1End = true;
+        playResultInfo.p1NickName = playInfo.nickName;
+        playResultInfo.p1Choice = playInfo.choice;
+      } else if (playInfo.playerNo === 2) {
+        playResultInfo.round = playInfo.round;
+        playResultInfo.nextRound = playInfo.round;
+        playResultInfo.isP2End = true;
+        playResultInfo.p2NickName = playInfo.nickName;
+        playResultInfo.p2Choice = playInfo.choice;
+      }
+
+      // 1,2번 모두 선택 완료시 라운드 점수계산
+      if (playResultInfo.isP1End && playResultInfo.isP2End) {
+        if (playResultInfo.p1Choice === "O" && playResultInfo.p2Choice === "O") {
+          playResultInfo.p1Score = 3;
+          playResultInfo.p2Score = 3;
+        } else if (playResultInfo.p1Choice === "O" && playResultInfo.p2Choice === "X") {
+          playResultInfo.p1Score = 0;
+          playResultInfo.p2Score = 5;
+        } else if (playResultInfo.p1Choice === "X" && playResultInfo.p2Choice === "O") {
+          playResultInfo.p1Score = 5;
+          playResultInfo.p2Score = 0;
+        } else if (playResultInfo.p1Choice === "X" && playResultInfo.p2Choice === "X") {
+          playResultInfo.p1Score = 1;
+          playResultInfo.p2Score = 1;
+        }
+        playResultInfo.nextRound = playInfo.round + 1; // 다음라운드
+        console.log("게임결과:", playResultInfo);
+        io.emit("play game", playResultInfo);
+        playResultInfo = { ...playResultInfo, ...playResultInit }; //결과전송후 round, nextRound 만 제외하고 클리어
+        console.log("게임결과 클리어:", playResultInfo);
+      } else {
+        io.emit("play game", playResultInfo);
+      }
     });
 
     /**
